be6edf13c9dc5a5a7d757130f511dd9795b85b27
Chris Flatley <chris@committed.io>
2019-09-11 11:09:40 +0100

    Fix dependency changes from core annot8

--- annot8-components-mongo/src/main/java/module-info.java
+++ annot8-components-mongo/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module io.annot8.components.mongo {
   requires io.annot8.conventions;
   requires transitive mongo.java.driver;
   requires com.google.common;
-  requires io.annot8.common.jackson.serialisation;
   requires com.fasterxml.jackson.databind;
   requires com.fasterxml.jackson.core;
   requires io.annot8.common.implementations;
