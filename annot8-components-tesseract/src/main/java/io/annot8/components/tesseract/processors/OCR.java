186fd8a65f812e6a36b8551c1ce989949148d190
Chris Flatley <chris@committed.io>
2019-09-12 18:06:46 +0100

    Fix tesseract module

--- annot8-components-tesseract/src/main/java/io/annot8/components/tesseract/processors/OCR.java
+++ annot8-components-tesseract/src/main/java/io/annot8/components/tesseract/processors/OCR.java
@@ -1,47 +1,34 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.tesseract.processors;
 
-import java.util.Arrays;
-import java.util.List;
-
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
-
-import org.apache.commons.io.FilenameUtils;
-
+import io.annot8.common.components.AbstractProcessor;
 import io.annot8.common.data.content.FileContent;
 import io.annot8.common.data.content.Text;
 import io.annot8.components.tesseract.processors.settings.TesseractSettings;
-import io.annot8.core.capabilities.CreatesContent;
-import io.annot8.core.capabilities.ProcessesContent;
-import io.annot8.core.components.Processor;
 import io.annot8.core.components.responses.ProcessorResponse;
-import io.annot8.core.context.Context;
 import io.annot8.core.data.Item;
-import io.annot8.core.exceptions.BadConfigurationException;
-import io.annot8.core.exceptions.MissingResourceException;
 import io.annot8.core.settings.Settings;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FilenameUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Takes FileContent containing either an image or PDF file, and produces a Text content with the
  * text from the file as extracted by Tesseract
  */
-@ProcessesContent(FileContent.class)
-@CreatesContent(Text.class)
-public class OCR extends AbstractComponent implements Processor {
+//@ProcessesContent(FileContent.class)
+//@CreatesContent(Text.class)
+public class OCR extends AbstractProcessor {
   private ITesseract instance = new Tesseract();
   private OCRSettings settings;
 
-  @Override
-  public void configure(Context context)
-      throws BadConfigurationException, MissingResourceException {
-    super.configure(context);
-    this.settings = context.getSettings(OCRSettings.class).orElseGet(OCRSettings::new);
-
-    if (!settings.validate()) throw new BadConfigurationException("Settings is not valid");
-
-    settings.getTesseractSettings().configureInstance(instance);
+  public OCR(OCRSettings settings) {
+   this.settings = settings;
+   settings.getTesseractSettings().configureInstance(instance);
   }
 
   @Override
@@ -58,13 +45,13 @@ public class OCR extends AbstractComponent implements Processor {
               try {
                 String content = instance.doOCR(fc.getData());
 
-                item.create(Text.class)
-                    .withName(fc.getName() + settings.getSuffix())
+                item.createContent(Text.class)
+                    .withDescription("OCR from "+ fc.getId() + settings.getSuffix())
                     .withData(content)
                     .withProperties(fc.getProperties())
                     .save();
               } catch (TesseractException e) {
-                log().error("Unable to extract text from content {}", fc.getName(), e);
+                log().error("Unable to extract text from content {}", fc.getId(), e);
               }
             });
 
@@ -72,7 +59,7 @@ public class OCR extends AbstractComponent implements Processor {
   }
 
   /** Settings class for {@link OCR} */
-  public static class OCRSettings extends AbstractComponent implements Settings {
+  public static class OCRSettings implements Settings {
 
     private final TesseractSettings tesseractSettings;
     private final List<String> extensions;
--- annot8-components-tesseract/src/test/java/io/annot8/components/tesseract/processors/OCRTest.java
+++ annot8-components-tesseract/src/test/java/io/annot8/components/tesseract/processors/OCRTest.java
@@ -1,30 +1,26 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.tesseract.processors;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.File;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
 import io.annot8.common.data.content.FileContent;
 import io.annot8.common.data.content.Text;
 import io.annot8.core.data.Item;
-import io.annot8.testing.testimpl.TestContext;
 import io.annot8.testing.testimpl.TestItem;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OCRTest {
 
   @Test
   @Disabled // Requires Tesseract to be correctly installed
   public void test() throws Exception {
-    OCR ocr = new OCR();
-    ocr.configure(new TestContext());
+    OCR ocr = new OCR(new OCR.OCRSettings());
 
     Item item = new TestItem();
-    item.create(FileContent.class)
-        .withName("test")
+    item.createContent(FileContent.class)
         .withData(new File(OCRTest.class.getResource("test-image.tif").toURI()))
         .save();
 
