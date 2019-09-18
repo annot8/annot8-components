1bcee5c0281dcef2fada0e52ecc32b32943fa88c
James Baker <jdbaker@dstl.gov.uk>
2019-09-18 18:24:53 +0100

    Update print processors

--- annot8-components-print/src/main/java/io/annot8/components/print/processors/PrintItem.java
+++ annot8-components-print/src/main/java/io/annot8/components/print/processors/PrintItem.java
@@ -2,56 +2,77 @@
 package io.annot8.components.print.processors;
 
 import io.annot8.api.annotations.Annotation;
+import io.annot8.api.bounds.Bounds;
+import io.annot8.api.capabilities.Capabilities;
 import io.annot8.api.components.responses.ProcessorResponse;
+import io.annot8.api.context.Context;
 import io.annot8.api.data.Content;
 import io.annot8.api.data.Item;
 import io.annot8.api.properties.Properties;
+import io.annot8.api.settings.NoSettings;
 import io.annot8.api.stores.AnnotationStore;
 import io.annot8.common.components.AbstractProcessor;
+import io.annot8.common.components.AbstractProcessorDescriptor;
+import io.annot8.common.components.capabilities.SimpleCapabilities;
 
-public class PrintItem extends AbstractProcessor {
+public class PrintItem extends AbstractProcessorDescriptor<PrintItem.Processor, NoSettings> {
 
   @Override
-  public ProcessorResponse process(Item item) {
-
-    println("---", 0);
-    println("Properties:", 0);
-    print(item.getProperties(), 1);
-    println("Content:", 0);
-    item.getContents().forEach(this::print);
-
-    return ProcessorResponse.ok();
+  protected Processor createComponent(Context context, NoSettings settings) {
+    return new Processor();
   }
 
-  private void print(Content<?> content) {
-    println(content.getId(), 1);
-    println("Properties:", 2);
-    print(content.getProperties(), 3);
-    println("Annotations:", 2);
-    print(content.getAnnotations(), 3);
+  @Override
+  public Capabilities capabilities() {
+    return new SimpleCapabilities.Builder()
+        .withProcessesContent(Content.class)
+        .withProcessesAnnotations("*", Bounds.class)
+        .build();
   }
 
-  private void print(Properties properties, int indent) {
-    properties
-        .getAll()
-        .forEach((key, value) -> println(String.format("%s: %s", key, value), indent));
-  }
+  public static class Processor extends AbstractProcessor {
+    @Override
+    public ProcessorResponse process(Item item) {
 
-  private void print(AnnotationStore annotations, int indent) {
-    annotations.getAll().forEach(a -> print(a, indent));
-  }
+      println("---", 0);
+      println("Properties:", 0);
+      print(item.getProperties(), 1);
+      println("Content:", 0);
+      item.getContents().forEach(this::print);
 
-  private void print(Annotation annotation, int indent) {
-    println(annotation.toString(), indent);
-  }
-
-  private void println(String s, int indent) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < indent; i++) {
-      sb.append(" ");
+      return ProcessorResponse.ok();
     }
-    sb.append(s);
 
-    System.out.println(sb.toString());
+    private void print(Content<?> content) {
+      println(content.getId(), 1);
+      println("Properties:", 2);
+      print(content.getProperties(), 3);
+      println("Annotations:", 2);
+      print(content.getAnnotations(), 3);
+    }
+
+    private void print(Properties properties, int indent) {
+      properties
+          .getAll()
+          .forEach((key, value) -> println(String.format("%s: %s", key, value), indent));
+    }
+
+    private void print(AnnotationStore annotations, int indent) {
+      annotations.getAll().forEach(a -> print(a, indent));
+    }
+
+    private void print(Annotation annotation, int indent) {
+      println(annotation.toString(), indent);
+    }
+
+    private void println(String s, int indent) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < indent; i++) {
+        sb.append(" ");
+      }
+      sb.append(s);
+
+      System.out.println(sb.toString());
+    }
   }
 }
--- annot8-components-print/src/main/java/io/annot8/components/print/processors/PrintSpans.java
+++ annot8-components-print/src/main/java/io/annot8/components/print/processors/PrintSpans.java
@@ -1,27 +1,47 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.components.print.processors;
 
-import java.util.Optional;
-
+import io.annot8.api.capabilities.Capabilities;
+import io.annot8.api.context.Context;
+import io.annot8.api.settings.NoSettings;
+import io.annot8.common.components.AbstractProcessorDescriptor;
+import io.annot8.common.components.capabilities.SimpleCapabilities;
 import io.annot8.common.data.bounds.SpanBounds;
 import io.annot8.common.data.content.Text;
 import io.annot8.components.base.processors.AbstractTextProcessor;
 
-public class PrintSpans extends AbstractTextProcessor {
+import java.util.Optional;
+
+public class PrintSpans extends AbstractProcessorDescriptor<PrintSpans.Processor, NoSettings> {
 
   @Override
-  protected void process(final Text content) {
+  protected Processor createComponent(Context context, NoSettings settings) {
+    return new Processor();
+  }
 
-    content
-        .getAnnotations()
-        .getByBounds(SpanBounds.class)
-        .forEach(
-            a -> {
-              final SpanBounds bounds = a.getBounds(SpanBounds.class).get();
-              final Optional<String> value = content.getText(a);
+  @Override
+  public Capabilities capabilities() {
+    return new SimpleCapabilities.Builder()
+        .withProcessesContent(Text.class)
+        .withProcessesAnnotations("*", SpanBounds.class)
+        .build();
+  }
 
-              System.out.println(
-                  String.format("Annotation %s: %s", bounds.toString(), value.orElse("UNKNOWN")));
-            });
+  public static class Processor extends AbstractTextProcessor {
+    @Override
+    protected void process(final Text content) {
+
+      content
+          .getAnnotations()
+          .getByBounds(SpanBounds.class)
+          .forEach(
+              a -> {
+                final SpanBounds bounds = a.getBounds(SpanBounds.class).get();
+                final Optional<String> value = content.getText(a);
+
+                System.out.println(
+                    String.format("Annotation %s: %s", bounds.toString(), value.orElse("UNKNOWN")));
+              });
+    }
   }
 }
