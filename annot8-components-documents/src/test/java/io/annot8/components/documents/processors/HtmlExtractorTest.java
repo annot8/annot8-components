3c9a5ec915594cbbec7ee691e2a52b7935a79eea
James Baker <jdbaker@dstl.gov.uk>
2021-05-06 10:41:15 +0100

    Test for internet connection prior to running tests and adjust tests as appropriate

--- annot8-components-documents/src/test/java/io/annot8/components/documents/processors/HtmlExtractorTest.java
+++ annot8-components-documents/src/test/java/io/annot8/components/documents/processors/HtmlExtractorTest.java
@@ -16,16 +16,53 @@ import io.annot8.conventions.PropertyKeys;
 import io.annot8.implementations.support.context.SimpleContext;
 import io.annot8.testing.testimpl.TestItem;
 import java.io.File;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HtmlExtractorTest extends AbstractDocumentExtractorTest {
+  private static final Logger LOG = LoggerFactory.getLogger(HtmlExtractorTest.class);
+  private boolean internet = false;
+
+  @BeforeAll
+  public void checkInternet() {
+    // Test for internet connection - as results may be different depending on connection
+    try {
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request =
+          HttpRequest
+              // Test using this image as it's one of the ones required in the testDocument.html
+              // file
+              .newBuilder(
+                  URI.create(
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Cosford-_Royal_Air_Force_Museum-_English_Electric_Lightning_suspended_from_the_ceiling_%28geograph_5765866%29.jpg/640px-Cosford-_Royal_Air_Force_Museum-_English_Electric_Lightning_suspended_from_the_ceiling_%28geograph_5765866%29.jpg"))
+              .build();
+
+      HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+
+      if (response.statusCode() == 200) internet = true;
+    } catch (Exception e) {
+      internet = false;
+    }
+
+    if (!internet) LOG.warn("Unable to download test image - assuming no internet connection");
+  }
+
   @Override
   protected Class<? extends AbstractDocumentExtractorDescriptor<?, HtmlExtractor.Settings>>
       getDescriptor() {
@@ -77,10 +114,14 @@ public class HtmlExtractorTest extends AbstractDocumentExtractorTest {
 
   @Override
   protected void validateImages(Collection<Image> imageContents) {
-    // FIXME: Broken on my machine - something not right with my SSL?
-    // FIXME: Test for internet connection before test?
 
-    /*assertEquals(3, imageContents.size());
+    if (internet) {
+      // Internet, so we should have 3 images
+      assertEquals(3, imageContents.size());
+    } else {
+      // No internet, so we should have just 1 image
+      assertEquals(1, imageContents.size());
+    }
 
     Comparator<Image> sortByIndex =
         (i1, i2) -> {
@@ -93,47 +134,54 @@ public class HtmlExtractorTest extends AbstractDocumentExtractorTest {
 
     List<Image> images = imageContents.stream().sorted(sortByIndex).collect(Collectors.toList());
 
-    Image image1 = images.get(0);
+    Image imageData;
+    if (internet) {
+      Image image1 = images.get(0);
 
-    assertNotNull(image1.getData());
-    assertTrue(image1.getData().getWidth() > 0);
-    assertTrue(image1.getData().getHeight() > 0);
-    assertEquals(1, image1.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
-    assertEquals(
-        "640px-Cosford-_Royal_Air_Force_Museum-_English_Electric_Lightning_suspended_from_the_ceiling_%28geograph_5765866%29.jpg",
-        image1.getProperties().get(PropertyKeys.PROPERTY_KEY_NAME).get());
-    assertEquals(
-        "Cosford - Royal Air Force Museum - English Electric Lightning suspended from the ceiling",
-        image1.getProperties().get(PropertyKeys.PROPERTY_KEY_TITLE).get());
-    assertEquals(
-        "Michael Garlick / Cosford: Royal Air Force Museum: English Electric Lightning suspended from the ceiling / CC BY-SA 2.0",
-        image1.getProperties().get(PropertyKeys.PROPERTY_KEY_DESCRIPTION).get());
-    assertEquals(
-        "English Electric Lightning suspended from the ceiling at RAF Museum Cosford",
-        image1.getProperties().get("html/alt").get());
+      assertNotNull(image1.getData());
+      assertTrue(image1.getData().getWidth() > 0);
+      assertTrue(image1.getData().getHeight() > 0);
+      assertEquals(1, image1.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
+      assertEquals(
+          "640px-Cosford-_Royal_Air_Force_Museum-_English_Electric_Lightning_suspended_from_the_ceiling_%28geograph_5765866%29.jpg",
+          image1.getProperties().get(PropertyKeys.PROPERTY_KEY_NAME).get());
+      assertEquals(
+          "Cosford - Royal Air Force Museum - English Electric Lightning suspended from the ceiling",
+          image1.getProperties().get(PropertyKeys.PROPERTY_KEY_TITLE).get());
+      assertEquals(
+          "Michael Garlick / Cosford: Royal Air Force Museum: English Electric Lightning suspended from the ceiling / CC BY-SA 2.0",
+          image1.getProperties().get(PropertyKeys.PROPERTY_KEY_DESCRIPTION).get());
+      assertEquals(
+          "English Electric Lightning suspended from the ceiling at RAF Museum Cosford",
+          image1.getProperties().get("html/alt").get());
 
-    Image image2 = images.get(1);
+      Image image2 = images.get(1);
 
-    assertNotNull(image2.getData());
-    assertTrue(image2.getData().getWidth() > 0);
-    assertTrue(image2.getData().getHeight() > 0);
-    assertEquals(2, image2.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
-    assertEquals(
-        "640px-Imperial_War_Museum_%28geograph_5302500%29.jpg",
-        image2.getProperties().get(PropertyKeys.PROPERTY_KEY_NAME).get());
-    assertFalse(image2.getProperties().has(PropertyKeys.PROPERTY_KEY_TITLE));
-    assertFalse(image2.getProperties().has(PropertyKeys.PROPERTY_KEY_DESCRIPTION));
+      assertNotNull(image2.getData());
+      assertTrue(image2.getData().getWidth() > 0);
+      assertTrue(image2.getData().getHeight() > 0);
+      assertEquals(2, image2.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
+      assertEquals(
+          "640px-Imperial_War_Museum_%28geograph_5302500%29.jpg",
+          image2.getProperties().get(PropertyKeys.PROPERTY_KEY_NAME).get());
+      assertFalse(image2.getProperties().has(PropertyKeys.PROPERTY_KEY_TITLE));
+      assertFalse(image2.getProperties().has(PropertyKeys.PROPERTY_KEY_DESCRIPTION));
 
-    Image image3 = images.get(2);
+      // Internet, so the data image is the last image in the document
+      imageData = images.get(2);
+    } else {
+      // No internet, so the only image is the data image
+      imageData = images.get(0);
+    }
 
-    assertNotNull(image3.getData());
-    assertTrue(image3.getData().getWidth() > 0);
-    assertTrue(image3.getData().getHeight() > 0);
-    assertEquals(3, image3.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
-    assertFalse(image3.getProperties().has(PropertyKeys.PROPERTY_KEY_NAME));
-    assertFalse(image3.getProperties().has(PropertyKeys.PROPERTY_KEY_TITLE));
-    assertFalse(image3.getProperties().has(PropertyKeys.PROPERTY_KEY_DESCRIPTION));
-    assertEquals("Rainbow", image3.getProperties().get("html/alt").get());*/
+    assertNotNull(imageData.getData());
+    assertTrue(imageData.getData().getWidth() > 0);
+    assertTrue(imageData.getData().getHeight() > 0);
+    assertEquals(3, imageData.getProperties().get(PropertyKeys.PROPERTY_KEY_INDEX).get());
+    assertFalse(imageData.getProperties().has(PropertyKeys.PROPERTY_KEY_NAME));
+    assertFalse(imageData.getProperties().has(PropertyKeys.PROPERTY_KEY_TITLE));
+    assertFalse(imageData.getProperties().has(PropertyKeys.PROPERTY_KEY_DESCRIPTION));
+    assertEquals("Rainbow", imageData.getProperties().get("html/alt").get());
   }
 
   @Override
